039501a3615be4838af22e7469546f1723d7a020
Denis <denistrator@yandex.ua>
2016-09-05 21:13:21 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-	// code
-})
+$( document ).ready(function() {		
+	var primary_Swiper = new Swiper('.primary-swiper-container', {
+	    speed: 400,
+	    spaceBetween: 450,
+	    // autoplay: 3500,
+
+	    pagination: '.primary-swiper-pagination',
+	    paginationClickable: true
+	});
+});
