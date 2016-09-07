89f7a269988676773b78af112e47afa760c9cc87
Denis <denistrator@yandex.ua>
2016-09-07 09:50:26 +0300

    add 'our works' and 'feedbacks' (with slider)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-$( document ).ready(function() {		
+$( document ).ready(function() {
+	$('.our-works__img-holder').height($('.our-works__elem').height() + 1);
+	$('.our-works__img-inner').height($('.our-works__elem').height() + 1);
+	$('.our-works__hover').height($('.our-works__elem').height() + 1);
+	$('.our-works__hover-inner').height($('.our-works__elem').height() + 1);
+
+
+
 	var primary_Swiper = new Swiper('.primary-swiper-container', {
 	    speed: 400,
 	    spaceBetween: 450,
@@ -6,5 +13,20 @@ $( document ).ready(function() {
 
 	    pagination: '.primary-swiper-pagination',
 	    paginationClickable: true
+	});
+
+	var primary_Swiper = new Swiper('.people-say-swiper-container', {
+	    speed: 400,
+	    spaceBetween: 450,
+	    // autoplay: 3500,
+
+	    nextButton: '.people-say-swiper-button-next',
+	    prevButton: '.people-say-swiper-button-prev',
+
+	    pagination: '.people-say-swiper-pagination',
+	    paginationClickable: true,
+	    paginationBulletRender: function (index, className) {
+            return '<span class="' + className + '">' + '<img src="images/content/slider_pagination/slider_pagination-' + (index + 1) + '.png">' + '</span>';
+        }
 	});
 });
